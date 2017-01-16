fe4d04a46774268f05d998b9353fe14946fa99e5
PeiDa Kuo <guo.peida@gmail.com>
2017-01-15 16:00:13 -0800

    configured routes for index and posts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
+import { IndexRedirect, Router, Route, browserHistory } from 'react-router';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../public/Reddit-icon.png';
+import Home from './components/Home.jsx';
+import PIndex from './components/PIndex.jsx';
+import Post from './components/Post.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router history={ browserHistory }>
+        <Route path="/" component={ Home }>
+          <IndexRedirect to="/index"/>
+          <Route path="/index" component={ PIndex } />
+          <Route path="/post/:postid" component={ Post } />
+        </Route>
+      </Router>
     );
   }
 }
