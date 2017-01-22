8662966843590bed6708e3f5e1032581968151d5
PeiDa Kuo <guo.peida@gmail.com>
2017-01-21 17:29:29 -0800

    separate HOT posts from subreddits posts

--- src/stores/PostStore.js
+++ src/stores/PostStore.js
@@ -16,6 +16,14 @@ class PostStore extends ReduceStore {
       case "HOT_POSTS_RECEIVED":
         return state.set(action.subreddit, action.posts);
 
+      case "POSTS_RECEIVED":
+        if (state.get("HOT")) {
+          let blankMap = state.clear();
+          return blankMap.set(action.subreddit, action.posts);
+        } else {
+          return state.set(action.subreddit, action.posts);
+        }
+
       default:
       return state;
     }
